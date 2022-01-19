75c69a6225170b6b804df90a01fb98109bbca120
2147152SaumyaThukral <96261459+2147152SaumyaThukral@users.noreply.github.com>
2022-01-20 00:15:02 +0530

    Update construction.js

--- construction.js
+++ construction.js
@@ -1,55 +1,189 @@
 /*--JS for DOM Parser--*/
 
-var xmlDoc
-var xmlFile ='construction.xml'
-
-//function to load xml doc
-function loadXML()
-{
-    var xmlReq = new XMLHttpRequest;
-    xmlReq.onreadystatechange = function()
-    {
-        console.log(xmlReq.readyState)
-        if((xmlReq.readyState == 4) && (xmlReq.status == 200))
-        {
-            //xml doc successfully retrieved
-            console.log(xmlReq.readyState)
-
-            xmlDoc = xmlReq.responseXML
-            displayTable()
-        }
-    }
-    xmlReq.open('GET',xmlFile, true)
-    xmlReq.send()
-}
-
-//function to display html table from xml data
-function displayTable()
-{
+function loadXML() {
+    var xmlReq = new XMLHttpRequest();
+  
+    console.log(xmlReq.readyState);
+  
+    xmlReq.onreadystatechange = function () {
+      console.log(xmlReq.readyState);
+      if (this.readyState == 4 && this.status == 200) {
+        // document.getElementById('customer_info').innerHTML = this.responseText;
+        cusinfo(this);
+        // console.log(renXml.readyState);
+      }
+    };
+  
+    xmlReq.open("GET", "construction.xml", "TRUE");
+  
+    xmlReq.send();
+  }
+  
+  var xmlDoc;
+  var table;
+  var x;
+  
+  function cusinfo(info) {
     var i;
-    var table = "<tr><th>Manager Name</th><th>Phone Number</th><th>Address</th><th>Email</th><th>Age</th><th>Projects</th></tr>";
-    var x = xmlDoc.getElementsByTagName("cd")
-    for (i = 0; i < x.length; i++)
-    {
-        table += "<tr><td>" +
-            x[i].getElementsByTagName("Managername")[0].childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Phone")[0].childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Address")[0].childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Email")[0].childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Age")[0].childNodes[0].nodeValue + "</td>" +
-            x[i].getElementsByTagName("Projects")[0].childNodes[0].nodeValue + "</td><td>" +
-            "<td><span class='material-icons' onclick='editRecord(" +i+ ")'>edit</span></td>" +
-            "<td><span class='material-icons' onclick='deleteRecord(" +i+ ")'>delete</span></td></tr>";
+    xmlDoc = info.responseXML;
+    // console.log(xmlDoc);
+    displayTable(xmlDoc);
+  }
+//function to display html table from xml data
+function displayTable(xmlDoc) {
+    table = `<thead><tr>
+    <th>Manager Name</th>
+    <th>Phone Number</th>
+    <th>Address</th>
+    <th>Email</th>
+    <th>Age</th>
+    <th>Projects</th>
+          </tr></thead>`;
+  
+    x = xmlDoc.getElementsByTagName("cd");
+  
+    for (i = 0; i < x.length; i++) {
+      table +=
+        "<tr><td>" +
+        x[i].getElementsByTagName("ManagerName")[0].childNodes[0].nodeValue +
+        "</td><td data-label='Date'>" +
+        x[i].getElementsByTagName("Phone")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Address")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Email")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Age")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Projects")[0].childNodes[0].nodeValue +
+        "</td><td><button onclick='removecus()'> remove </button></td></tr>";
     }
-    document.getElementById("table").innerHTML = table
-}
+    document.getElementById("table").innerHTML = table;
+  }
 
+function removecus() {
+    // console.log(xmlDoc.getElementsByTagName('customer')[0]);
+    // var x = xmlDoc.getElementsByTagName("customer")[0];
+  
+    var index,
+      table = document.getElementById("table");
+  
+    for (var i = 0; i < table.rows.length; i++) {
+      table.rows[i].onclick = function () {
+        index = this.rowIndex;
+  
+        x = xmlDoc.getElementsByTagName("Managername")[index - 1];
+        xmlDoc.documentElement.removeChild(x);
+        console.log(xmlDoc);
+        displayTable(xmlDoc);
+      };
+    }
+    alert("This detail removed!");
+  }
+  
+  function removeNodeElement() {
+    var i;
+  
+    for (i = 0; i < x.length; i++) {
+      a = xmlDoc
+        .getElementsByTagName("Managername")
+        [i].getElementsByTagName("Address")[0];
+      b = a.childNodes[0];
+      a.removeChild(b);
+    }
+    console.log(xmlDoc);
+    table = `<thead><tr>
+    <th>Manager Name</th>
+   <th>Phone Number</th>
+   <th>Address</th>
+   <th>Email</th>
+   <th>Age</th>
+   <th>Projects</th>
+  </tr></thead>`;
+  var some = xmlDoc;
+  x = xmlDoc.getElementsByTagName("Managername");
+  
+  for (i = 0; i < x.length; i++) {
+  table +=
+  "<tr><td>" +
+  x[i].getElementsByTagName("Manager Name")[0].childNodes[0].nodeValue +
+  "</td><td>" +
+  x[i].getElementsByTagName("Phone Number")[0].childNodes[0].nodeValue +
+  "</td><td>" +
+  x[i].getElementsByTagName("Address")[0].childNodes[0].nodeValue +
+  "</td><td>" +
+  x[i].getElementsByTagName("Email")[0].childNodes[0].nodeValue +
+  "</td><td>" +
+  x[i].getElementsByTagName("Age")[0].childNodes[0].nodeValue +
+  "</td><td>" +
+  x[i].getElementsByTagName("Projects")[0].childNodes[0].nodeValue +
+  "</td><td><button onclick='removecus()'> remove </button></td></tr>";
+  }
+  document.getElementById("table").innerHTML = table;
+    alert('The node element  "Address" has been removed!');
+  
+    
+  }
+  
+  function changeNodeValue() {
+    var j;
+    for (j = 0; j < x.length; j++) {
+      xmlDoc
+        .getElementsByTagName("Managername")
+        [j].getElementsByTagName("Projects")[0].childNodes[0].nodeValue = "ongoing";
+    }
+    console.log(xmlDoc);
+    alert("The projects status has been changed ongoing!");
+    displayTable(xmlDoc);
+  }
+  
+  function addNewElement() {
+    var i;
+    newEle = xmlDoc.createElement("other_info");
+    newText = xmlDoc.createTextNode("some info");
+    newEle.appendChild(newText);
+    xmlDoc.getElementsByTagName("Managername")[0].appendChild(newEle);
+    console.log(xmlDoc);
+    alert('The new node element "other info" has been added to the second node!');
+    var table = `<thead><tr>
+            <th>Manager Name</th>
+           <th>Phone Number</th>
+           <th>Address</th>
+           <th>Email</th>
+           <th>Age</th>
+           <th>Projects</th>
+           <th>Other info</th>
+           <th>Remove customer</th>
+          </tr></thead>`;
+  
+    x = xmlDoc.getElementsByTagName("Managername");
+  
+    for (i = 0; i < x.length; i++) {
+      table +=
+        "<tr><td>" +
+        x[i].getElementsByTagName("Manager Name")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Phone Number")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Address")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Email")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Age")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Projects")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[0].getElementsByTagName("other_info")[0].childNodes[0].nodeValue +
+        "</td><td><button onclick='removecus()'> remove </button></td></tr>";
+    }
+    document.getElementById("table").innerHTML = table;
+  }
 //function to delete record from XML
 function deleteRecord(i)
 {
     y = xmlDoc.getElementsByTagName("cd")[i]
     var name = y.getElementsByTagName("Managername")[0].childNodes[0].nodeValue
-    var reply = confirm("Do you want to delete record? \nName: " + name)
+    var reply = confirm("Do you want to delete record? \nManager Name: " + name)
     if(reply == true)
     {
         xmlDoc.documentElement.removeChild(y)
